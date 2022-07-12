a5bfa85a0cae6277f3e176d868e5fcce2e773cca
IvanSanczewski <isanczewski@gmail.com>
2022-07-12 21:37:40 +0300

    Ej 6 no reconoce genre (array dentro de otro array) al hacer .filter()

--- src/films.js
+++ src/films.js
@@ -1,4 +1,4 @@
-const movies = require("./data");
+// const movies = require("./data");
 
 // copia del array film con 22 películas
 const films = [
@@ -25,159 +25,188 @@ const films = [
     duration: '3h 22min',
     genre: ['Crime', 'Drama'],
     score: 9
-  },
-  {
-    title: 'The Dark Knight',
-    year: 2008,
-    director: 'Christopher Nolan',
-    duration: '2h 32min',
-    genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-    score: 9
-  },
-  {
-    title: '12 Angry Men',
-    // year: 1957,
-    year: 1994,
-    director: 'Sidney Lumet',
-    duration: '1h 36min',
-    genre: ['Crime', 'Drama'],
-    score: 8.9
-  },
-  {
-    title: 'Schindler"s List',
-    // year: 1993,
-    year: 1994,
-    director: 'Steven Spielberg',
-    duration: '3h 15min',
-    genre: ['Biography', 'Drama', 'History'],
-    score: 8.9
-  },
-  {
-    title: 'Pulp Fiction',
-    year: 1994,
-    director: 'Quentin Tarantino',
-    duration: '2h 34min',
-    genre: ['Crime', 'Drama'],
-    score: 8.9
-  },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    // year: 2003,
-    year: 1994,
-    director: 'Peter Jackson',
-    duration: '3h 21min',
-    genre: ['Adventure', 'Drama', 'Fantasy'],
-    score: 8.9
-  },
-  {
-    title: 'Il buono, il brutto, il cattivo',
-    // year: 1966,
-    year: 1994,
-    director: 'Sergio Leone',
-    duration: '3h 2min',
-    genre: ['Western'],
-    score: 8.9
-  },
-  {
-    title: 'Fight Club',
-    year: 1999,
-    director: 'David Fincher',
-    duration: '2h 19min',
-    genre: ['Drama'],
-    score: 8.8
-  },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-    director: 'Peter Jackson',
-    duration: '2h 58min',
-    genre: ['Adventure', 'Drama', 'Fantasy'],
-    score: 8.8
-  },
-  {
-    title: 'Forrest Gump',
-    year: 1994,
-    director: 'Robert Zemeckis',
-    duration: '2h 22min',
-    genre: ['Comedy', 'Drama', 'Romance'],
-    score: 8.8
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-    director: 'Irvin Kershner',
-    duration: '2h 4min',
-    genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
-    score: 8.8
-  },
-  {
-    title: 'Inception',
-    year: 2010,
-    director: 'Christopher Nolan',
-    duration: '2h 28min',
-    genre: ['Action', 'Adventure', 'Sci-Fi', 'Thriller'],
-    score: 8.8
-  },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-    director: 'Peter Jackson',
-    duration: '2h 59min',
-    genre: ['Adventure', 'Drama', 'Fantasy'],
-    score: 8.7
-  },
-  {
-    title: 'One Flew Over the Cuckoo"s Nest',
-    year: 1975,
-    director: 'Milos Forman',
-    duration: '2h 13min',
-    genre: ['Drama'],
-    score: 8.7
-  },
-  {
-    title: 'Goodfellas',
-    // year: 1990,
-    year: 1994,
-    director: 'Martin Scorsese',
-    duration: '2h 26min',
-    genre: ['Crime', 'Drama'],
-    score: 8.7
-  },
-  {
-    title: 'The Matrix',
-    // year: 1999,
-    year: 1994,
-    director: 'Lana Wachowski',
-    duration: '2h 16min',
-    genre: ['Action', 'Sci-Fi'],
-    score: 8.7
-  },
-  {
-    title: 'Shichinin no samurai',
-    year: 1954,
-    director: 'Akira Kurosawa',
-    duration: '3h 27min',
-    genre: ['Adventure', 'Drama'],
-    score: 8.7
-  },
-  {
-    title: 'Star Wars',
-    year: 1977,
-    director: 'George Lucas',
-    duration: '2h 1min',
-    genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
-    score: 8.7
-  },
-  {
-    title: 'Cidade de Deus',
-    // year: 2002,
-    year: 1994,
-    director: 'Fernando Meirelles',
-    duration: '2h 10min',
-    genre: ['Crime', 'Drama'],
-    score: 8.7
-  }  
+  }
 ];
+
+// const films = [
+//   {
+//     title: 'The Shawshank Redemption',
+//     year: 1994,
+//     director: 'Frank Darabont',
+//     duration: '2h 22min',
+//     genre: ['Crime', 'Drama'],
+//     score: 9.3
+//   },
+//   {
+//     title: 'The Godfather',
+//     year: 1972,
+//     director: 'Francis Ford Coppola',
+//     duration: '2h 55min',
+//     genre: ['Crime', 'Drama'],
+//     score: 9.2
+//   },
+
+
+//   {
+//     title: 'The Godfather: Part II',
+//     year: 1974,
+//     director: 'Francis Ford Coppola',
+//     duration: '3h 22min',
+//     genre: ['Crime', 'Drama'],
+//     score: 9
+//   },
+//   {
+//     title: 'The Dark Knight',
+//     year: 2008,
+//     director: 'Christopher Nolan',
+//     duration: '2h 32min',
+//     genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+//     score: 9
+//   },
+//   {
+//     title: '12 Angry Men',
+//     // year: 1957,
+//     year: 1994,
+//     director: 'Sidney Lumet',
+//     duration: '1h 36min',
+//     genre: ['Crime', 'Drama'],
+//     score: 8.9
+//   },
+//   {
+//     title: 'Schindler"s List',
+//     // year: 1993,
+//     year: 1994,
+//     director: 'Steven Spielberg',
+//     duration: '3h 15min',
+//     genre: ['Biography', 'Drama', 'History'],
+//     score: 8.9
+//   },
+//   {
+//     title: 'Pulp Fiction',
+//     year: 1994,
+//     director: 'Quentin Tarantino',
+//     duration: '2h 34min',
+//     genre: ['Crime', 'Drama'],
+//     score: 8.9
+//   },
+//   {
+//     title: 'The Lord of the Rings: The Return of the King',
+//     // year: 2003,
+//     year: 1994,
+//     director: 'Peter Jackson',
+//     duration: '3h 21min',
+//     genre: ['Adventure', 'Drama', 'Fantasy'],
+//     score: 8.9
+//   },
+//   {
+//     title: 'Il buono, il brutto, il cattivo',
+//     // year: 1966,
+//     year: 1994,
+//     director: 'Sergio Leone',
+//     duration: '3h 2min',
+//     genre: ['Western'],
+//     score: 8.9
+//   },
+//   {
+//     title: 'Fight Club',
+//     year: 1999,
+//     director: 'David Fincher',
+//     duration: '2h 19min',
+//     genre: ['Drama'],
+//     score: 8.8
+//   },
+//   {
+//     title: 'The Lord of the Rings: The Fellowship of the Ring',
+//     year: 2001,
+//     director: 'Peter Jackson',
+//     duration: '2h 58min',
+//     genre: ['Adventure', 'Drama', 'Fantasy'],
+//     score: 8.8
+//   },
+//   {
+//     title: 'Forrest Gump',
+//     year: 1994,
+//     director: 'Robert Zemeckis',
+//     duration: '2h 22min',
+//     genre: ['Comedy', 'Drama', 'Romance'],
+//     score: 8.8
+//   },
+//   {
+//     title: 'Star Wars: Episode V - The Empire Strikes Back',
+//     year: 1980,
+//     director: 'Irvin Kershner',
+//     duration: '2h 4min',
+//     genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+//     score: 8.8
+//   },
+//   {
+//     title: 'Inception',
+//     year: 2010,
+//     director: 'Christopher Nolan',
+//     duration: '2h 28min',
+//     genre: ['Action', 'Adventure', 'Sci-Fi', 'Thriller'],
+//     score: 8.8
+//   },
+//   {
+//     title: 'The Lord of the Rings: The Two Towers',
+//     year: 2002,
+//     director: 'Peter Jackson',
+//     duration: '2h 59min',
+//     genre: ['Adventure', 'Drama', 'Fantasy'],
+//     score: 8.7
+//   },
+//   {
+//     title: 'One Flew Over the Cuckoo"s Nest',
+//     year: 1975,
+//     director: 'Milos Forman',
+//     duration: '2h 13min',
+//     genre: ['Drama'],
+//     score: 8.7
+//   },
+//   {
+//     title: 'Goodfellas',
+//     // year: 1990,
+//     year: 1994,
+//     director: 'Martin Scorsese',
+//     duration: '2h 26min',
+//     genre: ['Crime', 'Drama'],
+//     score: 8.7
+//   },
+//   {
+//     title: 'The Matrix',
+//     // year: 1999,
+//     year: 1994,
+//     director: 'Lana Wachowski',
+//     duration: '2h 16min',
+//     genre: ['Action', 'Sci-Fi'],
+//     score: 8.7
+//   },
+//   {
+//     title: 'Shichinin no samurai',
+//     year: 1954,
+//     director: 'Akira Kurosawa',
+//     duration: '3h 27min',
+//     genre: ['Adventure', 'Drama'],
+//     score: 8.7
+//   },
+//   {
+//     title: 'Star Wars',
+//     year: 1977,
+//     director: 'George Lucas',
+//     duration: '2h 1min',
+//     genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+//     score: 8.7
+//   },
+//   {
+//     title: 'Cidade de Deus',
+//     // year: 2002,
+//     year: 1994,
+//     director: 'Fernando Meirelles',
+//     duration: '2h 10min',
+//     genre: ['Crime', 'Drama'],
+//     score: 8.7
+//   }  
+// ];
 
 
 // console.log(typeof allDirectors, allDirectors);
@@ -232,8 +261,15 @@ function moviesAverageOfDirector(array, director) {
   const directorMoviesScoresSum = directorMoviesScores.reduce((accumulatedScore, actualScore) => accumulatedScore + actualScore, 0); //returns a number with the sum of all the numbers from the array that was reduced
   let result = parseFloat((directorMoviesScoresSum/directorMovies.length).toFixed(2));
   console.log("EXERCICE 3 ->", result);
+  
+  
+  console.log(typeof directorMoviesScoresSum, directorMoviesScoresSum);
+  console.log(typeof result, result);
+
+
   return result;
 }
+moviesAverageOfDirector(films, 'Francis Ford Coppola');
 
 
 // Excercise 4
@@ -286,17 +322,17 @@ function orderByYear(array) {
   //   if (a.year > z.year) return 1;
   //   return 0;
   // });
-  // const moviesByYearAscendent = moviesByYear.reverse();  
 
 
-const moviesByYear = newArray.sort((function(a, z) { //the sort method returns the previous array in alphabetical order
-  if (a.title< z.title) return -1; //Next three lines prevent possible sorting errors due to numbers and misspellings such as capital & lowercase characters wrongly introduced in the database
-  if (a.title> z.title) return 1;
-})).sort(function(a, z) { //the sort method returns the previous array in alphabetical order
-  if (a.year < z.year) return -1; //Next three lines prevent possible sorting errors due to numbers and misspellings such as capital & lowercase characters wrongly introduced in the database
-  if (a.year > z.year) return 1;
-  return 0;
-});
+
+  const moviesByYear = newArray.sort((function(a, z) { //the sort method returns the previous array in alphabetical order
+    if (a.title < z.title ) return -1; //Next three lines prevent possible sorting errors due to numbers and misspellings such as capital & lowercase characters wrongly introduced in the database
+    if (a.title > z.title ) return 1;
+  })).sort(function(a, z) { //the sort method returns the previous array in alphabetical order
+    if (a.year < z.year) return -1; //Next three lines prevent possible sorting errors due to numbers and misspellings such as capital & lowercase characters wrongly introduced in the database
+    if (a.year > z.year) return 1;
+    return 0;
+  });
 
 
   // console.log(typeof abcMovies, abcMovies);
@@ -304,7 +340,7 @@ const moviesByYear = newArray.sort((function(a, z) { //the sort method returns t
   // console.log(typeof moviesByYearAscendent, moviesByYearAscendent);
 
 
-  // console.log("EXERCICE 5 ->", moviesByYearAscendent);
+  console.log("EXERCICE 5 ->", moviesByYear);
   // console.log(typeof moviesByYear);
   // return moviesByYearAscendent;
   return moviesByYear;
@@ -324,9 +360,22 @@ const moviesByYear = newArray.sort((function(a, z) { //the sort method returns t
 // console.log(typeof xxx, xxx );
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
+function moviesAverageByCategory(array, genre) {
+  const allGenreFilms = array.filter(item => item.genre === genre);
+  const genreScores = allGenreFilms.map(item => item.score);
+  const genreTotalScore = genreScores.reduce((accumulatedFilmScores, actualFilmScore) => accumulatedFilmScores + actualFilmScore, 0);
+  let genreAverageSocre = parseFloat((genreTotalScore / allGenreFilms.length).toFixed(2));
 
+  console.log(typeof allGenreFilms, allGenreFilms);
+  console.log(typeof genreScores, genreScores);
+  console.log(typeof genreTotalScore, genreTotalScore);
+  console.log(typeof genreAverageSocre, genreAverageSocre);
+
+
+  console.log("EXERCICE 5 ->", genreAverageSocre);
+  return genreAverageSocre;
 }
+moviesAverageByCategory(films, 'Crime' );
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
